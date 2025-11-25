d8e7c06d09c6d9b5669e23df8967a0ea5cc94740
Omaly123 <124527870+Omaly123@users.noreply.github.com>
2025-11-25 14:42:49 +0100

    better movement

--- player.gd
+++ player.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-const WEIGHT = 1
+const WEIGHT = 1.5
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
 var bullet_speed = 1000
+var airtime:float = 0
 @export var bullet: PackedScene
-
 
 
 
@@ -14,24 +14,39 @@ func _process(delta: float) -> void:
 		fire()
 
 func _physics_process(delta: float) -> void:
+	velocity -= 0.3*velocity * delta
 	# Add the gravity.
 	if not is_on_floor():
+		airtime += delta
 		if is_on_wall():
 			velocity += get_gravity() * delta * WEIGHT
 		else:
 			velocity += get_gravity() * delta * WEIGHT
+	else: airtime = 0
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
+	if Input.is_action_pressed("jump") and is_on_wall():
+		velocity.y = 2*JUMP_VELOCITY/3
+	
+	if not is_on_floor() and Input.is_action_pressed("down"):
+		velocity += delta*Vector2(0,4000)
+	
+	if not is_on_floor() and Input.is_action_pressed("jump"):
+		if airtime <= 20: velocity -= delta*Vector2(0,600)
+		velocity -= delta*Vector2(0,400)
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
+	if direction and velocity.x < SPEED and -SPEED < velocity.x:
+		velocity.x += delta * direction * SPEED * 10
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if velocity.x != 0 and is_on_floor(): velocity.x -= velocity.x/5
+		else: if velocity.x != 0: velocity.x -= velocity.x/50
+		
 
 	move_and_slide()
 
